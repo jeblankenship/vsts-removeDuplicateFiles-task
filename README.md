5841a9e8a2e1315918f3fb6639b3c06845a62c25
James Blankenship <jeblankenship@gmail.com>
2016-08-29 09:37:40 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remove Duplicate File(s) Task
+Build task for VS Team Services that deletes files in destination folder if they exist in the source folder.
+
+
+08/28/2016: This task is not ready for general public use yet.
---
+++ task/remove-duplicateFiles.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()]
+param(
+    [string] $sourceFolder,
+    [string] $destinationFolder
+)
+
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
+Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
+
+Write-Verbose "Entering script $($MyInvocation.MyCommand.Name)"
+Write-Verbose "Parameter Values"
+$PSBoundParameters.Keys | % { Write-HOST "  ${_} = $($PSBoundParameters[$_])" }
+if($sourceFolder -eq $destinationFolder){
+    Write-Error "Source and Destination folders cannot be the same"
+    return
+}
+
+$sourceFilenames = (Get-ChildItem $sourceFolder).Name
+if(!$sourceFilenames){
+   Write-Warning "No files found in source folder: $sourceFolder"
+   return
+}
+$Matches = Get-ChildItem $destinationFolder\*.* -include $sourceFilenames
+foreach ($File in $Matches){ write-host “Deleting File $File” -foregroundcolor “Red”; Remove-Item $File | out-null }
